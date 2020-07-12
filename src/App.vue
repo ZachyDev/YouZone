57906afa443d86061f80e59a499ccef4e7043c06
ZachyDev <mosetizachary001@gmail.com>
2020-07-13 02:20:02 +0300

    communicated with the child component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <SearchBar @termChange="onTermChange"></SearchBar>
-    <VideoList />
+    <VideoList v-bind:videos="videos"/>
     {{ videos.length }}
   </div>
 </template>
@@ -10,7 +10,7 @@
 import axios from 'axios';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
-const API_KEY = 'AIzaSyBo1RI4oxEqtZe2AmgmbMr-Lwq4AqpEkEE';
+const API_KEY = 'AIzaSyB5GqrHRaFYAJf1yUcipmZIWMRc8Rci5wE';
 export default {
   name: 'App',
   data() {
@@ -33,7 +33,7 @@ export default {
         }
       })
       .then(response => {
-        this.videos = response.data.items
+       this.videos = response.data.items;
       })
     }
   }
