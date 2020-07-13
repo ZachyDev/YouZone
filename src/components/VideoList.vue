56bcbf967c1a59078662b79e0984f6e359dcb521
ZachyDev <mosetizachary001@gmail.com>
2020-07-14 00:27:05 +0300

    created a video list item component

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,15 +1,19 @@
 <template>
  <div>
+   Videos Count: {{ videos.length}}
   <ul v-bind:key="video.etag" v-for="video in videos">
-     {{ video.snippet.channelTitle }}
-    {{ video.snippet.description}}
+     <VideoListItem></VideoListItem>
   </ul>
  </div>
 </template>
 
 <script>
+import VideoListItem from './VideoListItem';
 export default {
     name: 'VideoList',
+    components: {
+      VideoListItem
+    },
     props: {
       videos: Array
     }
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li>  Video list item</li>
+</template>
+
+<script>
+export default {
+    name: 'VideoListItem'
+}
+</script>
+
+<style>
+
+</style>
