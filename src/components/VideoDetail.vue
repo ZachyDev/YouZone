c5ad8f801f5da4ed4dfbc5d777fe16400ce375cb
ZachyDev <mosetizachary001@gmail.com>
2020-07-15 00:16:43 +0300

    applied styling to the video detail component

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,6 +1,9 @@
 <template>
   <div v-if="video">
-      {{ video.snippet.title}}
+      <div class="details">
+      <h4>{{ video.snippet.title}}</h4>
+      <p> {{ video.snippet.description }} </p>
+      </div>
   </div>
 </template>
 
@@ -11,6 +14,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.details{
+    margin-top:10px;
+    padding: 10px;
+    border: 1px solid #000;
+    border-radius: 4px;
+}
 </style>
